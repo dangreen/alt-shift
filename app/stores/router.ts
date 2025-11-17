0566d66d1a1c116ce68cbf5c07fb8577afdf1077
dangreen <danon0404@gmail.com>
2025-11-17 19:23:59 +0400

    fix: github pages spa support

--- app/stores/router.ts
+++ app/stores/router.ts
@@ -22,3 +22,12 @@ export const [$location, navigation] = import.meta.env.STORYBOOK
 export const $prevRoute = previous($location.$route)
 
 export const $applicatioId = routeParam($location, 'applicationId')
+
+if (import.meta.env.VITE_GITHUB_PAGES) {
+  const redirectTo = sessionStorage.getItem('redirect')
+
+  if (redirectTo) {
+    navigation.replace(redirectTo)
+    sessionStorage.removeItem('redirect')
+  }
+}
--- app/vite-env.d.ts
+++ app/vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly STORYBOOK: string
   readonly VITE_OPENAI_API_KEY: string
+  readonly VITE_GITHUB_PAGES: string
 }
 
 interface ImportMeta {
